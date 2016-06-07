485d4943e482422cdf3ade566833c061e1c9d531
Julián García <juliangdev@gmail.com>
2016-06-07 16:15:22 +0200

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,74 @@
+# -*- coding: UTF-8 -*-
+
+help = '*Comandos disponibles:*\n\n'
+help += '/fraseneruda - Frase célebre de Pablo Neruda\n'
+help += '/frasefranklin - Frase célebre de Benjamin Franklin\n'
+help += '/frasebonnard - Frase célebre de Pierre Bonnard\n'
+help += '/frasepopular - Frase célebre popular\n'
+help += '/frasedeportista - Frase célebre de deportistas\n'
+help += '/frasecoelho - Frase célebre de Paulo Coelho\n'
+help += '/frasegandhi - Frase célebre de Mahatma Gandhi\n'
+help += '/fraserandom - Frase célebre aleatoria\n'
+
+ayudafotos = '*Comandos disponibles:*\n\n'
+ayudafotos += '/neruda - Foto de Pablo Neruda\n'
+ayudafotos += '/franklin - Foto de Benjamin Franklin\n'
+ayudafotos += '/bonnard - Foto de Pierre Bonnard\n'
+ayudafotos += '/coelho - Foto de Paulo Coelho\n'
+ayudafotos += '/gandhi - Foto de Mahatma Gandhi\n'
+
+ayudagifs = '*Comandos disponibles:*\n\n'
+ayudagifs += '/deporte - Ejercicio\n'
+ayudagifs += '/exito - Exito\n'
+ayudagifs += '/alimentacion - Salud\n'
+
+neruda = [
+  "El triunfo del verdadero hombre surge de las cenizas del error. *(Pablo Neruda)*",
+  "Nunca te quejes del ambiente o de los que te rodean. Hay quienes en tu mismo ambiente supieron vencer. Las circunstancias son buenas o malas según la voluntad o fortaleza de tu corazón. *(Pablo Neruda)*",
+  "No te quejes de tu pobreza, de tu soledad o de tu suerte. Enfrenta con valor y acepta que de una u otra manera son el resultado de tus actos y la prueba que has de ganar. *(Pablo Neruda)*",
+  "No te amargues con tu propio fracaso ni se lo cargues a otro. Acéptate ahora o seguirás justificándote como un niño. Recuerda que cualquier momento es bueno para comenzar y que ninguno es tan terrible para claudicar. *(Pablo Neruda)*",
+  "Deja ya de engañarte. Eres la causa de ti mismo, de tu necesidad, de tu fracaso. *(Pablo Neruda)*"
+]
+
+franklin = [
+  "Recibirás un Cuerpo. Puede gustarte o no, pero será tuyo durante todo el tiempo que estés aquí. *(Bejamin Franklin)*",
+  "Aprenderás Lecciones. Estás inscrito en una escuela informal de tiempo completo llamada vida. *(Bejamin Franklin)*",
+  "Es posible que las lecciones te gusten o que te parezcan irrelevantes y tontas, pero no hay errores, sólo lecciones. *(Bejamin Franklin)*",
+  "El crecimiento es un proceso de prueba y error: es una experimentación. Los experimentos fallidos forman parte del proceso en igual medida que el experimento que funciona bien. *(Bejamin Franklin)*",
+  "No hay nada en la vida que no contenga sus lecciones. Si estás vivo, siempre tendrás algo para aprender. *(Bejamin Franklin)*"
+]
+
+bonnard = [
+  "Nada muere más rápidamente que una idea en una mente cerrada. *(Pierre Bonnard)*",
+  "La clave de tu futuro está escondida en tu vida diaria. *(Pierre Bonnard)*",
+  "Cuanto más mires hacia atrás, más difícil te resultará mirar hacia delante. *(Pierre Bonnard)*",
+  "El mejor momento del día es ahora. *(Pierre Bonnard)*",
+  "Nunca conseguirás seguir adelante si siempre piensas en la venganza. *(Pierre Bonnard)*"
+]
+
+popular = [
+  "Antes de desear algo ardientemente conviene comprobar la felicidad que le alcanza a quien ya lo posee. *(Popular)*",
+  "Nada es fácil; nada se regala en este mundo, todo tiene que aprenderse con mucho esfuerzo. Un hombre que va en busca del conocimiento debe comportarse de la misma manera que un soldado que va a la guerra: bien despierto, con miedo, con respeto, y con absoluta confianza. Siguiendo estos requisitos, podrá perder alguna que otra batalla, pero nunca se lamentará de su destino. *(Popular)*",
+  "Cuando deseas algo, todo el universo conspira para que realices tu deseo. *(Popular)*",
+  "El miedo es natural: el miedo a la libertad que nos da el conocimiento es absolutamente natural. No obstante, por terrible que sea el aprendizaje, es peor vivir sin sabiduría. *(Popular)*",
+  "Bendito el hombre que no teniendo nada que decir se abstiene de demostrarlo con sus palabras. *(Popular)*"
+]
+
+deportista = [
+  "Una vez que algo es una pasión, hay motivación. *(Michael Schumacher)*",
+  "Siempre pensé que los records estaban para ser rotos. *(Michael Schumacher)*",
+  "Soy un pensador muy positivo y creo que es lo que me ha ayudado en los momentos más difíciles. *(Roger Federer)*",
+  "Siempre juegas para ganar. Por eso juegas al tenis, porque amas el deporte e intentas ser lo mejor que puedes. *(Roger Federer)*",
+  "Tienes que esperar cosas de ti mismo antes de poder hacerlas. *(Michael Jordan)*"
+]
+
+coelho = [
+  "Todos los días Dios nos da un momento en que es posible cambiar todo lo que nos hace infelices. El instante mágico es el momento en que un sí o un no pueden cambiar toda nuestra existencia. *(Paulo Coelho)*",
+  "Nadie está a salvo de las derrotas. Pero es mejor perder algunos combates en la lucha por nuestros sueños, que ser derrotado sin saber siquiera por qué se está luchando. *(Paulo Coelho)*"
+]
+
+gandhi = [
+  "Una persona usualmente se convierte en aquello que el cree que es. Si yo sigo diciéndome a mi mismo que no puedo hacer algo, es posible que yo termine siendo incapaz de hacerlo. Por el contrario si yo tengo la creencia que sí puedo hacerlo, con seguridad yo adquiriré la capacidad de realizarlo aunque no la haya tenido al principio. *(Mahatma Gandhi)*"
+]
+
+todas=neruda+franklin+bonnard+popular+deportista+coelho+gandhi
